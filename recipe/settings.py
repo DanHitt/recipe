bb929bbc8e782a3e4167e3ec48caa61d5931e9b0
Dan the Man <d.hitt424@gmail.com>
2016-02-12 12:45:38 -0700

    More for Heroku

--- recipe/settings.py
+++ recipe/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
