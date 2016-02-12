24c5b001e8e14d1399b688998eb3331b79278c1a
Dan the Man <d.hitt424@gmail.com>
2016-02-12 10:03:14 -0700

    speeled requirements correctly

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	
 	quotes = ['"One should eat to live, not live to eat." --Moliere','"Food for the body is not enough. There must be food for the soul." --Dorothy Day','“One cannot think well, love well, sleep well, if one has not dined well.” ― Virginia Woolf','“Ask not what you can do for your country. Ask what’s for lunch.” ― Orson Welles','"Once, during Prohibition, I was forced to live for days on nothing but food and water."--W. C. Fields']
 	var randomNumber 
+	var moveLeft = -60;
+	var moveDown = 10;
 	
 	
 	$('.centerText').hover(function(){
@@ -10,6 +12,14 @@ $(document).ready(function(){
 		toastr.remove()
 		toastr.info(quotes[randomNumber]);
 	})
-	$('*nav')
+
+	$('#menu').hover(function(e){
+		$("#nav").show()
+			.css('top', e.pageY + moveDown)
+			.css('left', e.pageX + moveLeft)
+			.appendTo('body');
+	}, function(){
+		$("#nav").fadeOut(2000);
+	});
 
 });
